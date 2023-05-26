a588295190329dcb8d2dd15e649f74c061c16b50
vitinhoqzy <vi083450@gmail.com>
2023-05-26 17:27:24 -0300

    Adicionei um fundo no Menu

--- src/view/ViewMenu.java
+++ src/view/ViewMenu.java
@@ -27,19 +27,21 @@ public class ViewMenu extends javax.swing.JFrame {
     private void initComponents() {
 
         jMenuItem1 = new javax.swing.JMenuItem();
+        jLabel1 = new javax.swing.JLabel();
+        lblFundo1 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenuItem2 = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
-        jMenuItem4 = new javax.swing.JMenuItem();
-        jMenuItem10 = new javax.swing.JMenuItem();
+        mnuClientes = new javax.swing.JMenu();
+        itmConsultarClientes = new javax.swing.JMenuItem();
+        itmAlterarClientes = new javax.swing.JMenuItem();
+        itmCancelarClientes = new javax.swing.JMenuItem();
+        itmRelatóriosClientes = new javax.swing.JMenuItem();
         jSeparator1 = new javax.swing.JPopupMenu.Separator();
-        jMenuItem5 = new javax.swing.JMenuItem();
-        jMenu2 = new javax.swing.JMenu();
-        jMenuItem6 = new javax.swing.JMenuItem();
-        jMenuItem7 = new javax.swing.JMenuItem();
-        jMenuItem8 = new javax.swing.JMenuItem();
-        jMenuItem9 = new javax.swing.JMenuItem();
+        itmPagamentoClientes = new javax.swing.JMenuItem();
+        mnuFuncionários = new javax.swing.JMenu();
+        itmCadastrarFuncionario = new javax.swing.JMenuItem();
+        itmConsultarFuncionario = new javax.swing.JMenuItem();
+        itmAlterarFuncionario = new javax.swing.JMenuItem();
+        itmExcluirFuncionario = new javax.swing.JMenuItem();
 
         jMenuItem1.setText("jMenuItem1");
 
@@ -47,52 +49,60 @@ public class ViewMenu extends javax.swing.JFrame {
         setTitle("Menu Funcionários");
         getContentPane().setLayout(null);
 
-        jMenu1.setBorder(null);
-        jMenu1.setText("Clientes");
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/image-removebg-preview_4.png"))); // NOI18N
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(130, 60, 390, 230);
 
-        jMenuItem2.setText("Consultar Pedidos");
-        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+        lblFundo1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/image.png"))); // NOI18N
+        getContentPane().add(lblFundo1);
+        lblFundo1.setBounds(-10, 0, 680, 370);
+
+        mnuClientes.setBorder(null);
+        mnuClientes.setText("Clientes");
+
+        itmConsultarClientes.setText("Consultar Pedidos");
+        itmConsultarClientes.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem2ActionPerformed(evt);
+                itmConsultarClientesActionPerformed(evt);
             }
         });
-        jMenu1.add(jMenuItem2);
+        mnuClientes.add(itmConsultarClientes);
 
-        jMenuItem3.setText("Alterar Pedidos");
-        jMenu1.add(jMenuItem3);
+        itmAlterarClientes.setText("Alterar Pedidos");
+        mnuClientes.add(itmAlterarClientes);
 
-        jMenuItem4.setText("Cancelar Pedidos");
-        jMenu1.add(jMenuItem4);
+        itmCancelarClientes.setText("Cancelar Pedidos");
+        mnuClientes.add(itmCancelarClientes);
 
-        jMenuItem10.setText("Relatórios");
-        jMenu1.add(jMenuItem10);
-        jMenu1.add(jSeparator1);
+        itmRelatóriosClientes.setText("Relatórios ");
+        mnuClientes.add(itmRelatóriosClientes);
+        mnuClientes.add(jSeparator1);
 
-        jMenuItem5.setText("Pagamento");
-        jMenu1.add(jMenuItem5);
+        itmPagamentoClientes.setText("Pagamento");
+        mnuClientes.add(itmPagamentoClientes);
 
-        jMenuBar1.add(jMenu1);
+        jMenuBar1.add(mnuClientes);
 
-        jMenu2.setText("Funcionários");
+        mnuFuncionários.setText("Funcionários");
 
-        jMenuItem6.setText("Cadastrar Funcionário");
-        jMenu2.add(jMenuItem6);
+        itmCadastrarFuncionario.setText("Cadastrar Funcionário");
+        mnuFuncionários.add(itmCadastrarFuncionario);
 
-        jMenuItem7.setText("Consultar Funcionário");
-        jMenuItem7.addActionListener(new java.awt.event.ActionListener() {
+        itmConsultarFuncionario.setText("Consultar Funcionário");
+        itmConsultarFuncionario.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem7ActionPerformed(evt);
+                itmConsultarFuncionarioActionPerformed(evt);
             }
         });
-        jMenu2.add(jMenuItem7);
+        mnuFuncionários.add(itmConsultarFuncionario);
 
-        jMenuItem8.setText("Alterar Funcionário");
-        jMenu2.add(jMenuItem8);
+        itmAlterarFuncionario.setText("Alterar Funcionário");
+        mnuFuncionários.add(itmAlterarFuncionario);
 
-        jMenuItem9.setText("Excluir Funcionários");
-        jMenu2.add(jMenuItem9);
+        itmExcluirFuncionario.setText("Excluir Funcionários");
+        mnuFuncionários.add(itmExcluirFuncionario);
 
-        jMenuBar1.add(jMenu2);
+        jMenuBar1.add(mnuFuncionários);
 
         setJMenuBar(jMenuBar1);
 
@@ -100,13 +110,13 @@ public class ViewMenu extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jMenuItem7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem7ActionPerformed
+    private void itmConsultarFuncionarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itmConsultarFuncionarioActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem7ActionPerformed
+    }//GEN-LAST:event_itmConsultarFuncionarioActionPerformed
 
-    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+    private void itmConsultarClientesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itmConsultarClientesActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem2ActionPerformed
+    }//GEN-LAST:event_itmConsultarClientesActionPerformed
 
     /**
      * @param args the command line arguments
@@ -144,19 +154,21 @@ public class ViewMenu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuItem itmAlterarClientes;
+    private javax.swing.JMenuItem itmAlterarFuncionario;
+    private javax.swing.JMenuItem itmCadastrarFuncionario;
+    private javax.swing.JMenuItem itmCancelarClientes;
+    private javax.swing.JMenuItem itmConsultarClientes;
+    private javax.swing.JMenuItem itmConsultarFuncionario;
+    private javax.swing.JMenuItem itmExcluirFuncionario;
+    private javax.swing.JMenuItem itmPagamentoClientes;
+    private javax.swing.JMenuItem itmRelatóriosClientes;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem10;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem3;
-    private javax.swing.JMenuItem jMenuItem4;
-    private javax.swing.JMenuItem jMenuItem5;
-    private javax.swing.JMenuItem jMenuItem6;
-    private javax.swing.JMenuItem jMenuItem7;
-    private javax.swing.JMenuItem jMenuItem8;
-    private javax.swing.JMenuItem jMenuItem9;
     private javax.swing.JPopupMenu.Separator jSeparator1;
+    private javax.swing.JLabel lblFundo1;
+    private javax.swing.JMenu mnuClientes;
+    private javax.swing.JMenu mnuFuncionários;
     // End of variables declaration//GEN-END:variables
 }
